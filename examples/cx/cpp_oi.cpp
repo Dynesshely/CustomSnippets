16800c48dba2408b326fa2433f0bec3710d85708
醉月酿星河 <catrol@qq.com>
2025-03-16 03:47:34 +0000

    feat: examples

---
+++ examples/cx/cpp_oi.cpp
@@ -0,0 +1,27 @@
+// ### Include Libraries
+// #include <bits/stdc++.h>
+
+
+// ### Constant Variables
+
+
+// ### Definitions on Stack
+
+
+void init(void) {
+  
+}
+
+void input(void) {}
+
+void process(void) {}
+
+void output(void) {}
+
+int main(void) {
+  init();
+  input();
+  process();
+  output();
+  return 0;
+}
